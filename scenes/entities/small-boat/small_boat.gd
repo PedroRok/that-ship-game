83dab1402d70507f62c4807633b33489226959bb
rok <rexblanegamer@gmail.com>
2024-12-20 21:03:25 -0300

    First Commit

---
+++ scenes/entities/small-boat/small_boat.gd
@@ -0,0 +1,44 @@
+extends RigidBody2D
+
+# Velocidade de movimento do personagem
+@export var speed: float = 200.0
+
+# Taxa de desaceleração
+@export var deceleration: float = 1.0
+
+# Ângulo máximo de inclinação em radianos (ex.: 0.2 rad = ~11.5 graus)
+@export var max_tilt_angle: float = 0.2
+
+@onready var sprite = $SmallBoat
+@onready var particle = $GPUParticles2D as GPUParticles2D
+
+func _physics_process(delta: float) -> void:
+	var direction = Vector2.ZERO
+
+	# Detecta entradas do teclado
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+		sprite.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+		sprite.flip_h = true
+
+	# Calcula a velocidade desejada
+	var target_velocity_x = direction.x * speed
+
+	# Suaviza a transição para a velocidade alvo usando `lerp`
+	linear_velocity.x = lerp(linear_velocity.x, target_velocity_x, deceleration * delta)
+	# Rotação com base na velocidade
+	rotation = -linear_velocity.x / speed * max_tilt_angle
+	
+	if (abs(linear_velocity.x) > 0.1):
+		particle.amount_ratio = 1
+		var material = particle.process_material as ParticleProcessMaterial
+		material.gravity.x = -direction.x * 90
+		if (direction.x > 0):
+			particle.transform.x.x = -13
+		else:
+			particle.transform.x.x = 13
+		pass
+	else:
+		particle.amount_ratio = 0
---
+++ scenes/game/scripts/water_spring.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+
+var velocity = 0;
+
+var force = 0;
+
+var height = 0;
+
+var target_height = 0;
+
+@onready var collision = $Area2D/CollisionShape2D
+
+var index = 0
+
+var motion_factor = 0.02
+
+signal splash(index, speed)
+
+
+func water_update(spring_constant, dampening):
+	height = position.y
+	
+	var x = height - target_height
+	
+	var loss = - dampening * velocity
+	
+	# lei de Hooke
+	force = -spring_constant * x + loss
+	
+	velocity += force
+	
+	position.y  += velocity
+	pass
+	
+func initialize(x_position, id):
+	height = position.y
+	target_height = position.y
+	velocity = 0
+	position.x = x_position
+	index = id 
+	
+func set_collision_width(value):
+	var extents = collision.shape.size
+	var new_extents = Vector2(value, extents.y)
+	collision.shape.size = new_extents
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if (is_instance_of(body, RigidBody2D)):
+		var bd = body as RigidBody2D
+		if (bd.linear_velocity.x > 0.2 || bd.linear_velocity.x < -0.2 ): 
+			var speed = 10 * motion_factor
+			emit_signal("splash", index, speed)
+		pass # Replace with function body.
