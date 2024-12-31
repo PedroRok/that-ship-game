2e0a03d43507e6adb5a82240125050f1b86b93c9
rok <rexblanegamer@gmail.com>
2024-12-31 19:08:50 -0300

    Testing sound system

--- scenes/entities/guns/scripts/gun.gd
+++ scenes/entities/guns/scripts/gun.gd
@@ -5,7 +5,11 @@ var team : int = -1
 
 signal fired_bullet(bullet : Bullet, position : Vector2, direction : Vector2)
 
-@onready var bullet = preload("res://scenes/entities/guns/bullet.tscn")
+@onready 
+var bullet = preload("res://scenes/entities/guns/bullet.tscn")
+
+@onready 
+var audio = $AudioStreamPlayer2D
 
 @onready
 var end_barrel = $EndBarrel
@@ -14,3 +18,5 @@ func shoot_gun(direction: Vector2):
 	var bullet_instance = bullet.instantiate()
 	bullet_instance.team = team
 	EventBus.bullet_fired(bullet_instance, end_barrel.global_position, direction)
+	audio.pitch_scale = randf_range(0.5, 1.5)
+	#audio.play(0)
