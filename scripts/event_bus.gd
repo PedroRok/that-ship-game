d80bb40b564489d0546a7a2a9e35520c29521231
rok <rexblanegamer@gmail.com>
2025-01-05 16:18:39 -0300

    Refactoring event bus

--- scripts/event_bus.gd
+++ scripts/event_bus.gd
@@ -4,7 +4,7 @@ signal fired_bullet(bullet : Bullet, direction : Vector2, position : Vector2, ro
 signal spawn_ship(ship_name : String)
 
 func bullet_fired(bullet : Bullet, direction : Vector2, position : Vector2, rotation : float):
-	emit_signal("fired_bullet", bullet, direction, position, rotation)
+	fired_bullet.emit(bullet, direction, position, rotation)
 	
 func ship_spawned(ship_name : String):
-	emit_signal("spawn_ship", ship_name)
+	spawn_ship.emit(ship_name)
