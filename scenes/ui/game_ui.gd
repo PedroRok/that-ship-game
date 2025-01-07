c08e7894912d4dc5ade5e395b58f696c41ba61aa
rok <rexblanegamer@gmail.com>
2025-01-06 22:40:02 -0300

    Starting a better hud

--- scenes/ui/game_ui.gd
+++ scenes/ui/game_ui.gd
@@ -5,11 +5,12 @@ extends Control
 var ship_manager : ShipManager
 
 @onready
-var buttons_container : HBoxContainer  = $MarginContainer/ButtonsContainer
+var buttons_container : HBoxContainer  = $MarginContainer/MarginContainer/VBoxContainer/ButtonsContainer
 
 func _ready() -> void:
 	for button in buttons_container.get_children():
-		button.connect("spawn_ship", Callable(self, "spawn_ship"))
+		if button.enabled:
+			button.connect("spawn_ship", Callable(self, "spawn_ship"))
 
 
 func spawn_ship(ship_name : String) -> void:
--- scenes/ui/ship_button.gd
+++ scenes/ui/ship_button.gd
@@ -14,8 +14,20 @@ var button : Button = $Button
 
 signal spawn_ship(ship_name : String)
 
+var enabled : bool = true
+
 func _ready() -> void:
 	var shader_material : ShaderMaterial = button.material as ShaderMaterial
+	
+	if !ship_name && !ship_sprite:
+		button.disabled = true
+		shader_material.set_shader_parameter("enabled", false)
+		enabled = false
+		return
+	else:
+		button.disabled = false
+		shader_material.set_shader_parameter("enabled", true)
+		enabled = true
 	shader_material.set_shader_parameter("reference_texture", ship_sprite)
 	button.icon = ship_sprite
 
